056e2aee48f0e8ae49083d44df5a729c08dfae23
fnngj <fnngj@126.com>
2019-12-21 18:05:17 +0800

    完成变量功能的创建/编辑

--- app_variable/urls.py
+++ app_variable/urls.py
@@ -4,5 +4,6 @@ from app_variable import views
 urlpatterns = [
     # 任务管理
     path('', views.variable_manage),
+    path('add_variable/', views.add_variable),
 
 ]
--- app_variable/views.py
+++ app_variable/views.py
@@ -7,6 +7,29 @@ def variable_manage(request):
     """
     变量管理
     """
-    if request.method == "GET":
-        variables = Variable.objects.all()
-        return render(request, "variable/list.html", {"variables": variables})
+    variables = Variable.objects.all()
+    return render(request, "variable/list.html", {"variables": variables})
+
+
+def add_variable(request):
+    """
+    添加变量
+    """
+    if request.method == "POST":
+        vid = request.POST.get("vid", "")
+        key = request.POST.get("key", "")
+        value = request.POST.get("value", "")
+        if key == "" or value == "":
+            return response(10102, "参数不能为空！")
+        
+        if vid == "0":
+            Variable.objects.create(key=key, value=value)
+            return response(message="创建成功")
+        else:
+            variable = Variable.objects.get(id=vid)
+            variable.key = key
+            variable.value = value
+            variable.save()
+            return response(message="保存成功")
+    else:
+        return response(10101, "请求方法错误")
