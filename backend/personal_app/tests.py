e8262ed3b0c6eb0c9734aaa5bc9211b279bca720
defnngj <defnngj@gmail.com>
2021-07-05 23:30:26 +0800

    feat: 增加接口测试

--- backend/personal_app/tests.py
+++ backend/personal_app/tests.py
@@ -2,7 +2,11 @@ from django.test import TestCase
 
 # Create your tests here.
 import requests
-header = {"token": "adfasdfasdf"}
-r = requests.get("http://127.0.0.1:8000/test/", headers=header)
 
-print(r.text)
+login = requests.post("http://127.0.0.1:8000/api/personal/v1/login/", data={"username": "admin", "password": "admin123456"})
+login_token = login.json()["data"]["Token"]
+print("token", login_token)
+
+header = {"token": login_token}
+user = requests.get("http://127.0.0.1:8000/api/personal/v1/user/", headers=header)
+print(user.json())
--- backend/personal_app/utils/custom_response.py
+++ backend/personal_app/utils/custom_response.py
@@ -8,19 +8,19 @@ class Error(Enum):
     子定义错误码 与 错误信息
     """
     USER_OR_PAWD_NULL = {'10010': '用户名密码为空'}
-    USER_OR_PAWD_ERROR = {'10010': '用户名密码错误'}
+    USER_OR_PAWD_ERROR = {'10011': '用户名密码错误'}
 
 
-def response(success=True, error={}, data=[]) -> Response:
+def response(success: bool = True, error: Error = {}, data: any = []) -> Response:
     """
-    自定义错误返回格式
+    自定义接口返回格式
     """
-    if error != {}:
-        error_code = list(error.keys())[0]
-        error_msg = list(error.values())[0]
-    else:
+    if error == {}:
         error_code = ""
         error_msg = ""
+    else:
+        error_code = list(error.keys())[0]
+        error_msg = list(error.values())[0]
 
     resp = {
         "success": success,
