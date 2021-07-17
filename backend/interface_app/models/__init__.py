68118802b53e4e7ebf6d12be3242b617f1e99b26
defnngj <defnngj@gmail.com>
2021-07-17 16:44:25 +0800

    feat: 抽取模型层

---
+++ backend/interface_app/models/__init__.py
@@ -0,0 +1,4 @@
+from .project import *
+from .module import *
+from .case import *
+from .task import *
--- backend/interface_app/models/project.py
+++ backend/interface_app/models/project.py
@@ -13,17 +13,3 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class Module(models.Model):
-    """
-    模块表
-    """
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    name = models.CharField("名称", max_length=100, null=False, default="")
-    describe = models.TextField("描述", default="")
-    update_time = models.DateTimeField("更新时间", auto_now=True)
-    create_time = models.DateTimeField("创建时间", auto_now_add=True)
-
-    def __str__(self):
-        return self.name
