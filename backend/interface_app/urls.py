3e28724b9e480a1062252968deda267facb4dbd8
defnngj <defnngj@gmail.com>
2021-08-21 20:37:19 +0800

    feat: 用例增加ViewSet 模式

--- backend/interface_app/urls.py
+++ backend/interface_app/urls.py
@@ -1,15 +1,26 @@
 from django.urls import path
-from interface_app.views import ProjectView
-from interface_app.views import ModuleView
-from interface_app.views import CaseView
-
+from rest_framework import routers
+from interface_app.views.project_view import ProjectView
+from interface_app.views.module_view import ModuleView
+from interface_app.views.case_view import CaseView, CaseViewSet
+from interface_app.views.task_view import TaskViewSet
 
 urlpatterns = [
+    # 项目管理
     path('v1/project/<int:pk>/', ProjectView.as_view()),
     path('v1/project/', ProjectView.as_view()),
+
+    # 模块管理
     path('v1/module/<int:pk>/', ModuleView.as_view()),
     path('v1/module/', ModuleView.as_view()),
 
-    path('v1/case/', CaseView.as_view()),
-    path('v1/case/<int:pk>/', CaseView.as_view())
+    # 用例管理
+    # path('v1/case/', CaseView.as_view()),
+    # path('v1/case/<int:pk>/', CaseView.as_view()),
 ]
+
+router = routers.SimpleRouter()
+router.register(r'v1/case', CaseViewSet)  # 用例管理
+router.register(r'v1/task', TaskViewSet)  # 任务管理
+
+urlpatterns += router.urls
--- backend/interface_app/views/case_view.py
+++ backend/interface_app/views/case_view.py
@@ -1,10 +1,94 @@
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView
 from common_app.utils import response, Error, response_fail
-from django.forms.models import model_to_dict
 from common_app.utils import Pagination
-from interface_app.models import Project, Module, TestCase
+from interface_app.models import TestCase
 from interface_app.serializers import CaseValidators, CaseSerializer
+from rest_framework.viewsets import ViewSet
+from rest_framework.decorators import action
+
+
+class CaseViewSet(ViewSet):
+    queryset = TestCase.objects.all()
+    serializer_class = CaseSerializer
+    authentication_classes = []
+
+    @action(methods=["post"], detail=False, url_path='create')
+    def case_create(self, request, *args, **kwargs):
+        """
+        添加一条用例
+        /api/interface/v1/case/
+        """
+        val = CaseValidators(data=request.data)
+        if val.is_valid():
+            val.save()
+        else:
+            return response_fail(val.errors)
+
+        return response(data=val.data)
+
+    @action(methods=["get"], detail=True, url_path='info')
+    def get_case(self, request, *args, **kwargs):
+        """
+        获得用例数据
+        /api/interface/v1/case/<pk>/info/
+        """
+        pk = kwargs.get("pk")
+        if pk is not None:
+            try:
+                case = TestCase.objects.get(id=pk, is_delete=False)
+                ser = CaseSerializer(instance=case, many=False)
+            except TestCase.DoesNotExist:
+                return response(error=Error.CASE_ID_NULL)
+            return response(data=ser.data)
+
+    @action(methods=["get"], detail=False, url_path='list')
+    def get_cases_list(self, request, *args, **kwargs):
+        """
+        获得用例数据
+        /api/interface/v1/case/list/
+        """
+        test_case = TestCase.objects.filter(is_delete=False).all()
+        pg = Pagination()
+        page_module = pg.paginate_queryset(queryset=test_case, request=request, view=self)
+        ser = CaseSerializer(instance=page_module, many=True)
+        data = {
+            "total": test_case.count(),
+            "caseList": ser.data
+        }
+        return response(data=data)
+
+    @action(methods=["put"], detail=False, url_path='update')
+    def update_case(self, request, *args, **kwargs):
+        """
+        更新一条用例
+        /api/interface/v1/case/update/
+        """
+        cid = request.data.get("id")
+        if cid is None:
+            return response(error=Error.CASE_ID_NULL)
+        case = TestCase.objects.get(pk=cid, is_delete=False)
+        val = CaseValidators(instance=case, data=request.data)
+        if val.is_valid():
+            val.save()
+        else:
+            return response_fail(val.errors)
+
+        return response(data=val.data)
+
+    @action(methods=["delete"], detail=True, url_path='delete')
+    def delete_case(self, request, *args, **kwargs):
+        """
+        删除用例
+        /api/interface/v1/case/<pk>/info/
+        """
+        pk = kwargs.get("pk")
+        if pk is None:
+            return response(error=Error.CASE_ID_NULL)
+        try:
+            TestCase.objects.get(id=pk, is_delete=False).update(is_delete=True)
+        except TestCase.DoesNotExist:
+            return response(error=Error.CASE_OBJECT_NULL)
+        return response()
 
 
 class CaseView(APIView):
@@ -16,16 +100,15 @@ class CaseView(APIView):
         """
         pk = kwargs.get("pk")
         if pk is not None:
-            print("/case/1/")
             try:
-                case = TestCase.objects.get(id=pk)
+                case = TestCase.objects.get(id=pk, is_delete=False)
                 ser = CaseSerializer(instance=case, many=False)
             except TestCase.DoesNotExist:
                 return response(error=Error.PROJECT_ID_NULL)
             return response(data=ser.data)
         else:
             print("/case/?page=1&size=10")
-            test_case = TestCase.objects.all()
+            test_case = TestCase.objects.filter(is_delete=False).all()
             pg = Pagination()
             page_module = pg.paginate_queryset(queryset=test_case, request=request, view=self)
             ser = CaseSerializer(instance=page_module, many=True)
@@ -54,7 +137,7 @@ class CaseView(APIView):
         cid = request.data.get("id")
         if cid is None:
             return response(error=Error.CASE_ID_NULL)
-        case = TestCase.objects.get(pk=cid)
+        case = TestCase.objects.get(pk=cid, is_delete=False)
         val = CaseValidators(instance=case, data=request.data)
         if val.is_valid():
             val.save()
@@ -71,8 +154,7 @@ class CaseView(APIView):
         if pk is None:
             return response(error=Error.CASE_ID_NULL)
         try:
-            case = TestCase.objects.get(id=pk)
-            case.delete()
+            TestCase.objects.get(id=pk, is_delete=False).update(is_delete=True)
         except TestCase.DoesNotExist:
             return response(error=Error.CASE_OBJECT_NULL)
         return response()
